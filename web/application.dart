31d3a5eb0eb0794be717339f482fdbe12e8c2d8c
Gérald Reinhart <gerald.reinhart@gmail.com>
2015-01-28 17:59:02 +0100

    Update application.dart

--- web/application.dart
+++ web/application.dart
@@ -25,7 +25,7 @@ class ConnectingDartisansApplication  extends Application{
                                              "#FF7415","#FFAB15","#FFC866","#FFDEA3", "#FFEDCC");
   
     static final Color GREY_BLUE_GREEN = new Color("#40494D","#7F8D94","#ABB3B8","#C3C7C9","#E3E3E3",
-                                             "#7D8A67","#939E82","#ACB89A","#CBD1C0","#DEE0DA" ) ;
+                                             "#4E5444","#939E82","#ACB89A","#CBD1C0","#DEE0DA" ) ;  
   
   final Logger log = new Logger('ConnectingDartisansApplication');
   
