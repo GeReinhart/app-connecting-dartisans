c27e0e21b6bd336ad7352c55e7fdda7d2ec15f9e
GeReinhart <gerald.reinhart@gmail.com>
2015-03-26 12:57:36 +0100

    do not filter on map

--- lib/client/elements/dartisans_map.dart
+++ lib/client/elements/dartisans_map.dart
@@ -35,11 +35,11 @@ class DartisansMap extends Object with Showable, ApplicationEventPassenger {
     _googleMap = new GMap(_map, mapOptions);
     geoLocation();
 
-    _googleMap.onBoundsChanged.listen((_) {
+ /*   _googleMap.onBoundsChanged.listen((_) {
       Bounds bounds = new Bounds(_googleMap.bounds.northEast.lat, _googleMap.bounds.northEast.lng,
           _googleMap.bounds.southWest.lat, _googleMap.bounds.southWest.lng);
       fireApplicationEvent(new DartisansApplicationEvent.mapChange(this, bounds));
-    });
+    });*/
   }
 
   @override
